8ae293fad5f6d0bce83fec0295da02cb5337d9af
tmgbedu@gmail.com <tmgbedu@gmail.com>
2018-01-05 00:45:49 +0545

    this is

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Aammui | Home page</title>
+  <title>PublicityNepal | Nepal's Number 1 news portal</title>
   <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
