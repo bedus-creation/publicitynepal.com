848a2e1fd4cda763ae0be0bf894dd0f3a24f2290
tmgbedu@gmail.com <tmgbedu@gmail.com>
2018-03-17 21:04:41 +0545

    this is

--- app/Http/Controllers/FrontEnd/SitemapController.php
+++ app/Http/Controllers/FrontEnd/SitemapController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\FrontEnd;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Post;
 
 class SitemapController extends Controller
 {
+
     public function index(Request $request){
-    	return view('seo.sitemap');
+    	$posts=Post::all();
+    	$content = view('seo.sitemap', ["posts"=>$posts]);
+        return response($content, 200)
+            ->header('Content-Type', 'text/xml');
     }
 }
--- resources/views/seo/sitemap.blade.php
+++ resources/views/seo/sitemap.blade.php
@@ -2,10 +2,19 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
   xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
   <url> 
-    <loc>http://www.example.com/foo.html</loc> 
+    <loc>http://publicitynepal.com</loc> 
     <image:image>
-       <image:loc>http://example.com/image.jpg</image:loc>
-       <image:caption>Dogs playing poker</image:caption>
+       <image:loc>http://example.com/images/logo.png</image:loc>
+       <image:caption>Publicitynepal.com's Logo</image:caption>
     </image:image>
   </url>
+  @foreach($posts as $item)
+  <url> 
+    <loc>http://publicitynepal.com/news/{{$item->slug}}</loc> 
+    <image:image>
+       <image:loc>http://publicitynepal.com/uploads/images/{{$item->featured_photo}}</image:loc>
+       <image:caption>{{$item->title}} Featured Images</image:caption>
+    </image:image>
+  </url>
+  @endforeach
 </urlset>
