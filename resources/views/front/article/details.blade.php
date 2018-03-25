54511c6ac13ebaff41a4a5a87884d9a336bb4997
tmgbedu@gmail.com <tmgbedu@gmail.com>
2018-03-25 18:44:07 +0545

    Detail page

--- resources/views/front/article/details.blade.php
+++ resources/views/front/article/details.blade.php
@@ -3,31 +3,31 @@
 @section('content')
 <script type="application/ld+json">
 	{
-	  "@context": "http://schema.org",
-	  "@type": "NewsArticle",
-	  "mainEntityOfPage": {
-		"@type": "WebPage",
-		"@id": "https://google.com/article"
-	  },
-	  "headline": "{{$post->title}}",
-	  "image": [
-			"{{$post->featured_photo}}"
-	   ],
-	  "datePublished": "{{$post->created_at}}",
-	  "dateModified": "{{$post->updated_at}}",
-	  "author": {
-		"@type": "Person",
-		"name": "Publicity-Nepal"
-	  },
-	   "publisher": {
-		"@type": "Organization",
-		"name": "Publicity-Nepal",
-		"logo": {
-		  "@type": "ImageObject",
-		  "url": "https://google.com/logo.jpg"
-		}
-	  },
-	  "description": "{{{$post->content}}}"
+		"@context": "http://schema.org",
+		"@type": "NewsArticle",
+		"mainEntityOfPage": {
+			"@type": "WebPage",
+			"@id": "https://google.com/article"
+		},
+		"headline": "{{$post->title}}",
+		"image": [
+		"{{$post->featured_photo}}"
+		],
+		"datePublished": "{{$post->created_at}}",
+		"dateModified": "{{$post->updated_at}}",
+		"author": {
+			"@type": "Person",
+			"name": "Publicity-Nepal"
+		},
+		"publisher": {
+			"@type": "Organization",
+			"name": "Publicity-Nepal",
+			"logo": {
+				"@type": "ImageObject",
+				"url": "https://google.com/logo.jpg"
+			}
+		},
+		"description": "{{{$post->content}}}"
 	}
 </script>
 <article class="container" id="details">
@@ -48,8 +48,13 @@
 	</div>
 </article>
 <style type="text/css">
-	#details img{
-		max-width: 100% !important;
+@media screen and (max-width: 768px){
+	#details {
+		padding: 0 !important;
 	}
+}
+#details img{
+	max-width: 100% !important;
+}
 </style>	
 @endsection
