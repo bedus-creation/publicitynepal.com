a89ae8f0c4cd16ba9a90a7f7c957cc29e6b0b4f9
Bedus Creation <tmgbedu@gmail.com>
2019-02-26 18:25:45 +0545

    Footer Content deleted

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,16 +3,6 @@
 		<div class="container">
 			<div class="row pt-4">
 				<div class="col-md-4 pb-2">
-					<p>
-						<img class="mb-logo" src="{{url('/images/p5.jpg')}}" width="100%" height="80">
-					<div>सूचना विभाग दर्ता नंः ३५१/०७३-७४</div>	
-					</p>
-					<ul class="list-unstyled">
-						<li>Putalisadak 32,Newplaza Kathmandu,</li>
-						<li>Email: publicitynepal@gmail.com </li>
-						<li>Phone: +977-1-4425436 ,+977-9818209080 </li>
-						<li>Web: www.publicitynepal.com</li>
-					</ul>
 				</div>
 				<div class="col-md-8">
 					<div class="row">
@@ -29,10 +19,7 @@
 								<li><a href="#"  class="text-muted">प्रयोगका सर्त</a></li>
 							</ul>
 						</div>
-						<div class="col-md-4">
-							<p><strong class="c_name pb-0">विज्ञापनका लागि</strong>
-								<div>9818209080, 9841876939,</br>publicitynepal.com@gmail.com</div>
-							</p>
+						<div class="col-md-4">	
 						</div>
 					</div>
 				</div>
@@ -48,13 +35,13 @@
 			<div class="row">
 				<div class="col-md-12 text-center">
 					<div class="mb-5">
-						<a href="https://www.facebook.com/publicitynepalnews/">
+						<a href="#">
 							<i class="fa fa-facebook"> </i>
 						</a>
-						<a href="https://www.youtube.com/channel/UCDnfENrRal_mVpcpPbEsuFA">
+						<a href="">
 							<i class="fa fa-youtube"> </i>
 						</a>
-						<a href="https://twitter.com/publicitynepal">
+						<a href="">
 							<i class="fa fa-twitter"> </i>
 						</a>
 						<a href="#">
@@ -67,6 +54,6 @@
 	</div>
 	<div id="footer-bottom" class="footer-copyright py-3 text-center">
 		© 2010-2018 Copyright:
-		<a href="{{url('/')}}"> PublicityNepal.com </a>
+		<a href="{{url('/')}}"> Bishowghatana.com </a>
 	</div>
 </footer>
